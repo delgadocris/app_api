706d9095be031d5d206936efa2dacbe0f0d4f436
delgadocris <37419901+delgadocris@users.noreply.github.com>
2023-04-25 19:52:32 -0400

    Add api sign_up for user registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root to: "home#index"
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      devise_scope :user do
+        post "sign_up", to: "registrations#create"
+      end
+    end
+  end
 end
