dc4a85dfd3ae7364f1bf6f9e81a3330185d8bf06
delgadocris <37419901+delgadocris@users.noreply.github.com>
2023-04-26 11:19:52 -0400

    Add api log_out

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::SessionsController < Devise::SessionsController
   before_action :sign_in_params, only: :create
   before_action :load_user, only: :create
+  before_action :valid_token, only: :destroy
+  skip_before_action :verify_signed_out_user, only: :destroy
 
   # Sign in
   def create
@@ -10,6 +12,13 @@ class Api::V1::SessionsController < Devise::SessionsController
     else
       json_response "Unauthorized", false, {}, :unauthorized
     end
+  end
+
+  # Log out
+  def destroy
+    sign_out @user
+    @user.generate_new_authentication_token
+    json_response "Log Out Succesfully", true, {}, :ok
   end
 
   private
@@ -24,6 +33,15 @@ class Api::V1::SessionsController < Devise::SessionsController
       return @user
     else
       json_response "User not found", false, {}, :not_found
+    end
+  end
+
+  def valid_token
+    @user = User.find_by authentication_token: request.headers["AUTH-TOKEN"]
+    if @user
+      return @user
+    else
+      json_response "Invalid Token", false, {}, :not_found
     end
   end
 end
